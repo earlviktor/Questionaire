acac7d586e149b61937768e94b1d904980142d1b
Vitia <Vitia@MacBook-Earl.local>
2012-01-27 20:06:23 +0300

    git add Question.h git add Question.m

---
+++ Question.h
@@ -0,0 +1,13 @@
+//
+//  Question.h
+//  Questionaire
+//
+//  Created by Earl Vik on 27.01.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Question : NSObject
+
+@end
---
+++ Question.m
@@ -0,0 +1,13 @@
+//
+//  Question.m
+//  Questionaire
+//
+//  Created by Earl Vik on 27.01.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Question.h"
+
+@implementation Question
+
+@end

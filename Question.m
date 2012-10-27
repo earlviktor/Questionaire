6bb06c4e63857484153c868deca8864f7009dfa6
earlviktor <earlvik@gmail.com>
2012-10-27 20:20:50 +0400

    Update Question.m

--- Question.m
+++ Question.m
@@ -10,4 +10,95 @@
 
 @implementation Question
 
+@synthesize testName;
+
+@synthesize questionNumber;
+
+@synthesize questionText;
+
+@synthesize questionType;
+
+@synthesize strInputAnswer;
+
+@synthesize strRightAnswer;
+
+@synthesize numOfAnswers;
+
+@synthesize answers;
+
+
+-(bool)isRight:(NSString *)value{
+
+    self.strInputAnswer=value;
+    
+  if ([self.strInputAnswer isEqualToString:self.strRightAnswer]) return YES;
+        
+	else return NO;
+        
+
+}
+
+
+-(void)initWithAnswers:(int)value 
+{
+    
+[self setQuestionType:value];
+
+    switch (self.questionType) {
+  
+      case 1:{
+           
+ [self setQuestionText:@"What is the color of leaves in summer"];
+  
+          [self setNumOfAnswers:-1];
+  
+          self.strRightAnswer=@"green";
+         
+   
+            }
+          
+  break;
+    
+        
+	case 2:{
+           
+ 	self.questionText=@"what is the color of leaves in summer";
+ 
+           NSArray* ar=[[NSArray alloc] initWithObjects:@"red",@"blue",@"green", nil];
+
+            [self setAnswers:ar];
+           
+ 	self.numOfAnswers=3;
+         
+   self.strRightAnswer=@"2";
+        
+
+        
+        }
+            
+      
+      break;
+        
+	case 3: {
+  
+          self.questionText=@"what can be the color of leaves in autumn";
+   
+         NSArray* ar=[[NSArray alloc] initWithObjects:@"orange",@"violet",@"red",@"pink",@"blue" nil];
+         
+	   self.answers=ar;
+          
+	  self.numOfAnswers=5;
+      
+	      self.strRightAnswer=@"5";//right answers are N0&N2 so strRightAnswer equals 2^0+2^2
+      
+      
+        }
+        
+    break;
+        
+   
+ }
+    
+}
+
 @end
